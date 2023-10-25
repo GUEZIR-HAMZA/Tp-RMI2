74cb6d487f7e568469c9ed77f608cd38ab917cd7
GUEZIR-HAMZA <95761219+GUEZIR-HAMZA@users.noreply.github.com>
2023-10-25 22:21:39 +0100

    Add files via upload
    FIRST COMMIT

---
+++ ClientRMI/src/clientrmi2/ClientRMI2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientrmi2;
+
+import dao.IDao;
+import entities.Machine;
+import entities.Salle;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Guezir
+ */
+public class ClientRMI2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            IDao<Machine> daoMachine = (IDao<Machine>) Naming.lookup("rmi://localhost:1099/Machine");
+            
+            IDao<Salle> daoSalle = (IDao<Salle>) Naming.lookup("rmi://localhost:1099/Salle");
+            System.out.println(daoSalle.create(new Salle("code")));
+            
+
+            /*for (Machine m : dao.findAll()) {
+                System.out.println(m);
+            }*/
+
+        } catch (NotBoundException ex) {
+            Logger.getLogger(ClientRMI2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(ClientRMI2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RemoteException ex) {
+            Logger.getLogger(ClientRMI2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
